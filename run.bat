59c023864a39fb67fcb18d6541e06e3351026514
Vinjn Zhang <vinjn@users.noreply.github.com>
2023-06-07 21:19:28 -0700

    Initial commit

---
+++ run.bat
@@ -0,0 +1,9 @@
+%~dp0\app\kit\omni.app.dev.bat ^
+    --ext-folder %~dp0\exts ^
+    --ext-folder %~dp0\app\exts ^
+    --ext-folder %~dp0\app\extscache ^
+    --enable omni.kit.profiler.window ^
+    --enable omni.kit.debug.vscode ^
+    --enable omni.kit.browser.sample ^    
+    --enable omni.hello.world ^
+    %*
